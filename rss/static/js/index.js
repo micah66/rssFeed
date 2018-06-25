c20e748119015cc0d4c853306780285b9ee34dfd
Micah Gordon <micahgordon66@gmail.com>
2018-06-25 15:33:17 +0300

    Add static files and templates

---
+++ rss/static/js/index.js
@@ -0,0 +1,25 @@
+const getFeed = () => {
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: '/rss/get_feed',
+    // url: 'http://localhost:8000/get_feed',
+    success: data => {
+      $('#cookie_counter').html(data['cookies'])
+      const headlines = $('<ul />')
+      for (let i = 0; i < 10; i++) {
+        headlines.append(`
+          <li><a href="${data['data'][i]['link']}" target="_blank"><h3>${data['data'][i]['title']}</h3></a>
+            <ul>
+              <li>Author: ${data['data'][i]['author']}</li>
+              <li>Published: ${data['data'][i]['published']}</li>
+              <li>${data['data'][i]['summary']}</li>
+            </ul>
+          </li>`)
+      }
+      $('#headlines').html(headlines)
+    }
+  })
+}
+getFeed()
+$('#refresh').click(getFeed)
